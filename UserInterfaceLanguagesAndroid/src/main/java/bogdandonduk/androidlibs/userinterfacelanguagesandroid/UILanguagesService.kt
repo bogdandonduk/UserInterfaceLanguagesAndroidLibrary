f6e087e14624f9130a4798198f280bbc09ea0f3e
bogdandonduk <bogdandonduk@gmail.com>
2021-04-26 11:39:55 +0700

    Commit 5

--- UserInterfaceLanguagesAndroid/src/main/java/bogdandonduk/androidlibs/userinterfacelanguagesandroid/UILanguagesService.kt
+++ UserInterfaceLanguagesAndroid/src/main/java/bogdandonduk/androidlibs/userinterfacelanguagesandroid/UILanguagesService.kt
@@ -4,28 +4,34 @@ import android.content.Context
 import android.content.SharedPreferences
 import android.content.res.Configuration
 import android.content.res.Resources
+import android.widget.TextView
+import androidx.annotation.StringRes
 import java.util.*
 
 object UILanguagesService {
     private const val delimiter = "_"
 
     private const val LIBRARY_PREFIX = "prefs" + delimiter + "bogdandonduk.androidlibs.userinterfacelanguagesandroid" + delimiter
-    private const val CURRENT_APP_LANGUAGE_CODE = "currentAppLanguage$delimiter$LIBRARY_PREFIX"
+    private const val APP_LANGUAGE_CODE = "appLanguage$delimiter$LIBRARY_PREFIX"
 
     private fun getPreferences(context: Context) : SharedPreferences =
         context.getSharedPreferences(LIBRARY_PREFIX + context.packageName, Context.MODE_PRIVATE)
 
-    private fun getCurrentAppLanguageCode(context: Context) : String =
+    fun getAppLanguageCode(context: Context) : String =
         getPreferences(context)
-            .getString(CURRENT_APP_LANGUAGE_CODE, Locale.getDefault().language)!!
+            .getString(APP_LANGUAGE_CODE, Locale.getDefault().language)!!
 
 
-    fun setLanguageCode(context: Context, languageCode: String, host: UILanguagesHost?) {
-        getPreferences(context).edit().putString(CURRENT_APP_LANGUAGE_CODE, languageCode).apply()
+    fun setAppLanguageCode(context: Context, languageCode: String, host: UILanguagesHost?) {
+        getPreferences(context).edit().putString(APP_LANGUAGE_CODE, languageCode).apply()
 
         host?.initializeLanguage()
     }
 
     private fun getConfiguredResources(context: Context, languageCode: String) : Resources =
         context.createConfigurationContext(Configuration(context.resources.configuration).apply { setLocale(Locale(languageCode)) }).resources
+
+    fun initializeTextLanguage(context: Context, text: TextView, @StringRes stringResId: Int) {
+        text.text = getConfiguredResources(context, getAppLanguageCode(context)).getString(stringResId)
+    }
 }
