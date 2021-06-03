c7ce561bd47822b672d62cf835f2fb6b1a3d4c6e
bogdandonduk <bogdandonduk@gmail.com>
2021-06-04 00:54:26 +0700

    Commit 18

--- UserInterfaceLanguagesAndroid/src/main/java/bogdandonduk/androidlibs/userinterfacelanguagesandroid/UILanguagesService.kt
+++ UserInterfaceLanguagesAndroid/src/main/java/bogdandonduk/androidlibs/userinterfacelanguagesandroid/UILanguagesService.kt
@@ -1,21 +1,10 @@
 package bogdandonduk.androidlibs.userinterfacelanguagesandroid
 
-import android.app.Activity
 import android.content.Context
 import android.content.SharedPreferences
 import android.content.res.Configuration
 import android.content.res.Resources
-import android.os.Build
-import android.text.Editable
-import android.view.MenuItem
-import android.widget.EditText
-import android.widget.TextView
 import androidx.annotation.StringRes
-import androidx.appcompat.widget.ActionMenuView
-import androidx.appcompat.widget.Toolbar
-import androidx.appcompat.widget.TooltipCompat
-import androidx.core.view.children
-import bogdandonduk.androidlibs.popuputilsandroid.PopupUtilsService
 import java.util.*
 
 object UILanguagesService {
@@ -43,20 +32,4 @@ object UILanguagesService {
 
     fun resolveStringResource(context: Context, @StringRes stringResId: Int, languageCode: String = getAppLanguageCode(context)) =
         getConfiguredResources(context, languageCode).getString(stringResId)
-
-    fun initializeTextLanguage(context: Context, text: TextView, @StringRes stringResId: Int) {
-        text.text = getConfiguredResources(context, getAppLanguageCode(context)).getString(stringResId)
-    }
-
-    fun initializeMenuItemTitleLanguage(context: Context, menuItem: MenuItem?, @StringRes stringResId: Int) {
-        menuItem?.title = getConfiguredResources(context, getAppLanguageCode(context)).getString(stringResId)
-    }
-
-    fun initializeEditTextLanguage(context: Context, editText: EditText, @StringRes stringResId: Int) {
-        editText.text = Editable.Factory.getInstance().newEditable(getConfiguredResources(context, getAppLanguageCode(context)).getString(stringResId))
-    }
-
-    fun initializeEditTextHintLanguage(context: Context, editText: EditText, @StringRes stringResId: Int) {
-        editText.hint = getConfiguredResources(context, getAppLanguageCode(context)).getString(stringResId)
-    }
 }
